89c6a66f3e4bbd1c07e6a7b9ab481cfc44fa36f7
Arica Ng <aricang@me.com>
2021-05-10 17:11:43 +0800

    initial setup for users/navbar/flashes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:address])
+  end
+
 end

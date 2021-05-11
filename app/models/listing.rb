c8f8a0d71e727f7fd58a35713adad844f110cdbb
anushka singh <anushka13singh@gmail.com>
2021-05-11 15:58:04 +0800

    new branch created

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,10 @@
 class Listing < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
+
+  validates :title, presence: true
+  validates :category, presence: true
+  validates :unit_price, presence: true
+  validates :quantity, presence: true
+  validates :expiry_date, presence: true
 end

5947e65d96b61ab9da58475a33ea2242a320a69d
Jack Zhang <junbojack@gmail.com>
2021-05-12 11:28:19 +0800

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'listings#index'
   get 'orders/index'
   get 'orders/show'
-  root to: 'listings#index'
   devise_for :users
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :listings do
     resources :order_items, only: [:create]
   end
